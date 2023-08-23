7cb4a5177e7893398e38cf21a1052f5f082bce87
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-23 21:09:50 +0700

    asd

--- learn-spring-framework/src/main/java/com/chanokchon/learnspringframework/App01GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/chanokchon/learnspringframework/App01GamingBasicJava.java
@@ -1,14 +1,14 @@
 package com.chanokchon.learnspringframework;
 
-import com.chanokchon.learnspringframework.game.SuperContraGame;
 import com.chanokchon.learnspringframework.game.GameRunner;
 import com.chanokchon.learnspringframework.game.MarioGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
-		var game = new MarioGame();
 		// var game = new SuperContraGame();
+		
+		var game = new MarioGame();
 		var gameRunner = new GameRunner(game);
 		
 		gameRunner.run();
---
+++ learn-spring-framework/src/main/java/com/chanokchon/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,9 @@
+package com.chanokchon.learnspringframework;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+
+	}
+
+}

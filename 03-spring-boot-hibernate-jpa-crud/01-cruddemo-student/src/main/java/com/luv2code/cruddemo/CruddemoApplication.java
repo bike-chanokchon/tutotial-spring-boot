00f92b52c3bda3be58b31ca3be9a8196a65a0b2a
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-08-27 17:11:44 +0700

    initial project

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,24 @@
+package com.luv2code.cruddemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	// executed after Spring beans have loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		// Java lambda expression
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}

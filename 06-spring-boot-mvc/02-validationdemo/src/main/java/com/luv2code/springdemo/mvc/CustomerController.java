9a162c531017dc41f7fe9e8478428d62fb070041
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-09-05 23:58:07 +0700

    asd

--- 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/customers")
+@RequestMapping("/customer")
 public class CustomerController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer()); // (name ที่จะให้ view เรียกใช้, value)
 

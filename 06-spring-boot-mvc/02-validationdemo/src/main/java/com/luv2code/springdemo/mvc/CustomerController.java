acd654784e1e2a68a031edf3a6027b5ef8eede0d
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-09-05 23:35:03 +0700

    Create CustomerController.java

---
+++ 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -0,0 +1,18 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("customer", new Customer()); // (name ที่จะให้ view เรียกใช้, value)
+
+        return "customer-form";
+    }
+}

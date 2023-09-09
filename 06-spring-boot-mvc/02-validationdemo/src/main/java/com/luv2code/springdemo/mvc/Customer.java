0a17fdcb8d35c809e07da64b77c5307d39d79185
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-09-09 16:40:55 +0700

    asd

--- 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,17 +1,27 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
 
-    
     @NotNull(message = "is required")
     @NotEmpty(message = "can not empty")
     @Size(min = 1, max = 10, message = "last name must be charactor size between 1 ~ 10")
     private String lastName = "";
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private Integer freePasses; // เปลี่ยนไปใช้ Integer แทน int เพื่อแก้ปัญหาไม่สามารถแปรง String เป็น int
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -27,6 +37,22 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 
 }
--- 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-mvc/02-validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,9 +1,12 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,17 @@ import jakarta.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add an initBinder ... to convert trim input strings
+    // remove leading and traling whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(false);
+
+        dataBinder.registerCustomEditor(String.class, "lastName", stringTrimmerEditor);
+    }
+
 
     @GetMapping("")
     public String showForm(Model model) {
@@ -26,10 +40,12 @@ public class CustomerController {
     // BindingResult เก็บผลลัพธ์จากการทำ validation,
     // ถ้าหากทุกอย่างถูกต้องจะมีข้อมูลนั้น
     @PostMapping("/processForm")
-    public String proceString(
-            @Valid @ModelAttribute("customer") Customer customer,
-            BindingResult bindingResult) {
-
+    public String proceString(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
+        
+        System.out.println("First name: |" + customer.getFirstName() + "|");
+        System.out.println("Last name: |" + customer.getLastName() + "|");
+        System.out.println("Binding result: " + bindingResult.toString());
+    
         if (bindingResult.hasErrors()) {
             return "customer-form";
         }

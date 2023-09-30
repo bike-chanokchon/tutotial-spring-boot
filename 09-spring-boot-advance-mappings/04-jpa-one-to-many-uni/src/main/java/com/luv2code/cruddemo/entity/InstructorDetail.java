22cdfe8ae8fa94044dc8d02c486dcb819f96552b
Chanokchon Wongjumpa <bike.chanokchon2001@gmail.com>
2023-09-30 08:33:22 +0700

    310

---
+++ 09-spring-boot-advance-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,85 @@
+package com.luv2code.cruddemo.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+// annotate the class as an rntity and map to db table
+@Entity // https://chat.openai.com/share/743ac4dc-81d4-4c0b-9955-0a12f92ecea0
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+    // define the field and annotate the fields with db column names
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    // cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,CascadeType.REFRESH }
+    // เมื่อทำการลบ instructor detail ออกแล้ว instructor จะไม่ถูกลบด้วย
+    @OneToOne(mappedBy = "instructorDetail", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH })
+    private Instructor instructor;
+
+    public InstructorDetail() {
+
+    }
+
+    // create constructors
+    @Autowired
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // generate getter/setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    // generate toString method
+    @Override
+    public String toString() {
+        return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+    }
+}
